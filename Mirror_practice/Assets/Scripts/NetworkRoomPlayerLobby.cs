7c60d2ec9a886de4c8bf5a96befa9a70afff39f6
pj-99 <62024696+pj-99@users.noreply.github.com>
2021-05-09 21:21:53 +0800

    Revert "Spawn player in fixed position"
    This reverts commit e6427df713a7ce2966fabeaa3b7375e3a02699a5.

--- Mirror_practice/Assets/Scripts/NetworkRoomPlayerLobby.cs
+++ Mirror_practice/Assets/Scripts/NetworkRoomPlayerLobby.cs
@@ -64,8 +64,8 @@ public class NetworkRoomPlayerLobby : NetworkBehaviour
         //check if is leader
         if (Room.RoomPlayers[0].connectionToClient != connectionToClient) return;
         //start game
-        Room.StartGame();
-        Debug.Log("romm.startgame!");
+        
+        Debug.Log("start game!");
     }
 
     public override void OnStartClient()
